dac438937a131b55b4626d3419003467a3c0fd3e
Robert Attfield <emailme@robrtattfield.com>
2015-02-26 07:44:23 +0000

    Successfully ran initial DB migrations for user table, added appropriate changs to .env file.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,13 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
+			$table->string('user_name')->unique();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
 			$table->string('email')->unique();
 			$table->string('password', 60);
+            $table->string('bitcoin_address')->unique();
+            $table->boolean('is_admin');
 			$table->rememberToken();
 			$table->timestamps();
 		});
