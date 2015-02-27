aeea44bbd3abba43ff2f0cfc528072085d16c8fe
Robert Attfield <emailme@robrtattfield.com>
2015-02-27 04:52:06 +0000

    Updated faucets migration to incluse min and max payouts.

--- database/migrations/2015_02_26_075050_create_faucets_table.php
+++ database/migrations/2015_02_26_075050_create_faucets_table.php
@@ -18,8 +18,9 @@ class CreateFaucetsTable extends Migration {
             $table->string('name');
             $table->string('url')->unique();
             $table->integer('interval_minutes');
-            $table->integer('payout_amount');
-            $table->boolean('has_ref_program');
+            $table->integer('min_payout');;
+            $table->integer('max_payout');
+            $table->boolean('has_ref_program')->default(0);
             $table->decimal('ref_payout_percent',5,2);
             $table->boolean('is_paused')->default(0);
 			$table->timestamps();
