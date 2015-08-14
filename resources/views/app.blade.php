eb321452d095412e0eae0b39f2836f817a2820a7
Rob Attfield <robert.attfield@eyemagnet.com>
2015-08-15 09:42:02 +1200

    Added Bing and Yandex verification.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,6 +24,8 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	<meta name = 'yandex-verification' content = '6bd366f4a927b8e4' />
+	<meta name="msvalidate.01" content="01CE0CA0B4512F8EF0B231C935E124E1" />
 </head>
 <body>
     @include('partials.nav')
