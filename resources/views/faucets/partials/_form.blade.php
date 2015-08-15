3abd797aaa226bb5a763f677e9cbf3b09a4ed142
Rob Attfield <emailme@robertattfield.com>
2015-08-15 06:39:20 -0400

    Removed max restriction on faucet referral percentage for payout, as some offer over 100%.

--- resources/views/faucets/partials/_form.blade.php
+++ resources/views/faucets/partials/_form.blade.php
@@ -71,7 +71,7 @@
 <div class="form-group">
     {!! Form::label('ref_payout_percent', 'Referral Payout (%):', ['class' => 'col-lg-2 control-label']) !!}
     <div class="col-lg-10">
-        {!! Form::number('ref_payout_percent', null, ['class' => 'form-control', 'min' => 0, 'max' => 100]) !!}
+        {!! Form::number('ref_payout_percent', null, ['class' => 'form-control', 'min' => 0]) !!}
     </div>
 </div>
 
