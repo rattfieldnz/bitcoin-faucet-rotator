47ca534eadbe44f053878b0d34a219d96034a813
Rob Attfield <emailme@robertattfield.com>
2015-08-31 22:16:15 -0400

    Removed Facebook follow button.

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -43,9 +43,6 @@
                         <a href="https://twitter.com/FreeBTCWebsite" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @FreeBTCWebsite</a>
                         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
                     </li>
-					<li style="margin-top:1em; margin-left:0.75em;">
-						<div class="fb-follow" data-href="https://www.facebook.com/freebtc.website" data-layout="button_count" data-show-faces="true"></div>
-					</li>
 					<li>
 						<div id="btc-quote"></div>
 						<script type="text/javascript" src="https://www.weusecoins.com/embed.js"></script>
