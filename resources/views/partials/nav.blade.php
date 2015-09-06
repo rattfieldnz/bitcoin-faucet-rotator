692e357f19b8434393564f4c04cd17e10bf156e8
Rob Attfield <emailme@robertattfield.com>
2015-09-06 18:19:21 -0400

    Updated CSS and navigation.

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -44,7 +44,7 @@
                         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
                     </li>
 					<li>
-						<div id="btc-quote"></div>
+						<div id="btc-quote" style="margin-left:1em;"></div>
 						<script type="text/javascript" src="https://www.weusecoins.com/embed.js"></script>
 					</li>
                 @endif
