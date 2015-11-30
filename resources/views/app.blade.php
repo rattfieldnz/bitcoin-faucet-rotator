b7dc7440281ecf9f9c35ef690ecb9cb6137897d6
Rob Attfield <emailme@robertattfield.com>
2015-12-01 08:16:17 +1300

    Removed redundant Facebook markup.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -29,15 +29,6 @@
         @yield('ckeditor-script')
 </head>
 <body>
-
-	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.4&appId=1506179922982508";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
 	
     @include('partials.nav')
 
