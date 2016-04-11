0f8a06f6fd144343f0096a85de4ca656af5c2afd
Rob Attfield <emailme@robertattfield.com>
2016-04-11 02:00:39 -0400

    Added favicon icon.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,6 +32,8 @@
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="/images/bitcoin-16x16.ico">
+
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
