924ed80a16e22428d2807b20880eba2333a825f2
Rob Attfield <emailme@robertattfield.com>
2016-09-23 14:02:29 +1200

    Pulled files from dev machine to local.

--- app/PaymentProcessor.php
+++ app/PaymentProcessor.php
@@ -6,28 +6,7 @@ use Cviebrock\EloquentSluggable\SluggableTrait;
 
 /**
  * Class PaymentProcessor
- *
  * @package App
- * @property integer $id
- * @property string $name
- * @property string $url
- * @property string $slug
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
- * @property string $meta_title
- * @property string $meta_description
- * @property string $meta_keywords
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Faucet[] $faucets
- * @method static \Illuminate\Database\Query\Builder|\App\PaymentProcessor whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\PaymentProcessor whereName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\PaymentProcessor whereUrl($value)
- * @method static \Illuminate\Database\Query\Builder|\App\PaymentProcessor whereSlug($value)
- * @method static \Illuminate\Database\Query\Builder|\App\PaymentProcessor whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\PaymentProcessor whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\PaymentProcessor whereMetaTitle($value)
- * @method static \Illuminate\Database\Query\Builder|\App\PaymentProcessor whereMetaDescription($value)
- * @method static \Illuminate\Database\Query\Builder|\App\PaymentProcessor whereMetaKeywords($value)
- * @mixin \Eloquent
  */
 class PaymentProcessor extends Model implements SluggableInterface
 {
