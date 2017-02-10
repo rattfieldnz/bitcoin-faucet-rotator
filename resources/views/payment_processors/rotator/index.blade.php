95941fd321accdca84b15aa7aa9cb3074fe67a23
Rob Attfield <emailme@robertattfield.com>
2017-02-10 22:05:18 +1300

    Re-designed faucet rotator navigation with glyphicons.

--- resources/views/payment_processors/rotator/index.blade.php
+++ resources/views/payment_processors/rotator/index.blade.php
@@ -11,18 +11,52 @@
 <span property="{{ $paymentProcessor->slug }}" id="faucet_slug"></span>
 <p id="comments"><small><a href="#disqus_thread">See comments</a></small></p>
 @include('partials.ads')
-<nav id="navcontainer">
-    <ul id="navlist">
-        <li><a class="btn btn-primary btn-lg" id="first_faucet" title="First Faucet" role="button">First</a></li>
-        <li><a class="btn btn-primary btn-lg" id="previous_faucet" title="Previous Faucet" role="button">Previous</a></li>
-        <li><a class="btn btn-primary btn-lg" id="current" href="" target="_blank" title="Current Faucet (opens in new window)" role="button">Current</a></li>
-        <li><a class="btn btn-primary btn-lg" id="reload_current" title="Reload Current Faucet" role="button">Reload Current</a></li>
-        <li><a class="btn btn-primary btn-lg" id="next_faucet" title="Next Faucet" role="button">Next</a></li>
-        <li><a class="btn btn-primary btn-lg" id="last_faucet" title="Last Faucet" role="button">Last</a></li>
-        <li><a class="btn btn-primary btn-lg" id="random" title="Random Faucet" role="button">Random</a></li>
-        <li><a class="btn btn-primary btn-lg" id="list_of_faucets" href="/payment_processors/{{ $paymentProcessor->slug }}" title="List of Faucets" role="button">List of Faucets</a></li>
-    </ul>
-</nav>
+<div class="row">
+    <nav id="nav-container" class="col-lg-12 center-block">
+        <ul id="nav-list">
+            <li>
+                <a class="btn btn-primary btn-lg" id="first_faucet" title="First Faucet" role="button">
+                    <span class="glyphicon glyphicon-fast-backward"></span>
+                </a>
+            </li>
+            <li>
+                <a class="btn btn-primary btn-lg" id="previous_faucet" title="Previous Faucet" role="button">
+                    <span class="glyphicon glyphicon-step-backward"></span>
+                </a>
+            </li>
+            <li>
+                <a class="btn btn-primary btn-lg" id="current" href="" target="_blank" title="Current Faucet (opens in new window)" role="button">
+                    <span class="glyphicon glyphicon-export"></span>
+                </a>
+            </li>
+            <li>
+                <a class="btn btn-primary btn-lg" id="reload_current" title="Reload Current Faucet" role="button">
+                    <span class="glyphicon glyphicon-refresh"></span>
+                </a>
+            </li>
+            <li>
+                <a class="btn btn-primary btn-lg" id="next_faucet" title="Next Faucet" role="button">
+                    <span class="glyphicon glyphicon-step-forward"></span>
+                </a>
+            </li>
+            <li>
+                <a class="btn btn-primary btn-lg" id="last_faucet" title="Last Faucet" role="button">
+                    <span class="glyphicon glyphicon-fast-forward"></span>
+                </a>
+            </li>
+            <li>
+                <a class="btn btn-primary btn-lg" id="random" title="Random Faucet" role="button">
+                    <span class="glyphicon glyphicon-random"></span>
+                </a>
+            </li>
+            <li>
+                <a class="btn btn-primary btn-lg" id="list_of_faucets" href="/faucets" title="List of Faucets" role="button">
+                    <span class="glyphicon glyphicon-list"></span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
 
 <iframe sandbox="allow-forms allow-scripts allow-pointer-lock allow-same-origin" src="" id="rotator-iframe"></iframe>
 
