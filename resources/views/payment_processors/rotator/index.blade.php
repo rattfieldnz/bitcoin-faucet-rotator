f18bc660c79a188a4559c0f04eaa51db12a1df52
Rob Attfield <emailme@robertattfield.com>
2017-02-10 22:19:37 +1300

    Moved rotator nav into partial view.

--- resources/views/payment_processors/rotator/index.blade.php
+++ resources/views/payment_processors/rotator/index.blade.php
@@ -11,52 +11,7 @@
 <span property="{{ $paymentProcessor->slug }}" id="faucet_slug"></span>
 <p id="comments"><small><a href="#disqus_thread">See comments</a></small></p>
 @include('partials.ads')
-<div class="row">
-    <nav id="nav-container" class="col-lg-12 center-block">
-        <ul id="nav-list">
-            <li>
-                <a class="btn btn-primary btn-lg" id="first_faucet" title="First Faucet" role="button">
-                    <span class="glyphicon glyphicon-fast-backward"></span>
-                </a>
-            </li>
-            <li>
-                <a class="btn btn-primary btn-lg" id="previous_faucet" title="Previous Faucet" role="button">
-                    <span class="glyphicon glyphicon-step-backward"></span>
-                </a>
-            </li>
-            <li>
-                <a class="btn btn-primary btn-lg" id="current" href="" target="_blank" title="Current Faucet (opens in new window)" role="button">
-                    <span class="glyphicon glyphicon-export"></span>
-                </a>
-            </li>
-            <li>
-                <a class="btn btn-primary btn-lg" id="reload_current" title="Reload Current Faucet" role="button">
-                    <span class="glyphicon glyphicon-refresh"></span>
-                </a>
-            </li>
-            <li>
-                <a class="btn btn-primary btn-lg" id="next_faucet" title="Next Faucet" role="button">
-                    <span class="glyphicon glyphicon-step-forward"></span>
-                </a>
-            </li>
-            <li>
-                <a class="btn btn-primary btn-lg" id="last_faucet" title="Last Faucet" role="button">
-                    <span class="glyphicon glyphicon-fast-forward"></span>
-                </a>
-            </li>
-            <li>
-                <a class="btn btn-primary btn-lg" id="random" title="Random Faucet" role="button">
-                    <span class="glyphicon glyphicon-random"></span>
-                </a>
-            </li>
-            <li>
-                <a class="btn btn-primary btn-lg" id="list_of_faucets" href="/faucets" title="List of Faucets" role="button">
-                    <span class="glyphicon glyphicon-list"></span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-</div>
+@include('partials.rotator_nav')
 
 <iframe sandbox="allow-forms allow-scripts allow-pointer-lock allow-same-origin" src="" id="rotator-iframe"></iframe>
 
