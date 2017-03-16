dfbc0c057c6fc22dd57ab645c299ca98227f9228
Rob Attfield <emailme@robertattfield.com>
2017-03-16 22:31:09 +1300

    Fixed editing form issue with payment processors.

--- resources/views/payment_processors/edit.blade.php
+++ resources/views/payment_processors/edit.blade.php
@@ -5,7 +5,7 @@
 
     @include('payment_processors/partials/_errors')
 
-    {!! Form::model( $paymentProcessor, ['method' => 'PATCH', 'route' => ['payment_processors.update', $paymentProcessor->id],'class' => 'form-horizontal']) !!}
+    {!! Form::model( $paymentProcessor, ['method' => 'PATCH', 'route' => ['payment_processors.update', $paymentProcessor->slug],'class' => 'form-horizontal']) !!}
     @include('payment_processors/partials/_form')
     {!! Form::close() !!}
 @endsection
