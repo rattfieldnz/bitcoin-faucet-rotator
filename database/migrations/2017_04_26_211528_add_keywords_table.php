9c517c005fff6ca00121d21fcd79e92b0ea2f3df
Rob Attfield <emailme@robertattfield.com>
2017-04-26 21:23:30 +1200

    Adding keywords table migration.

---
+++ database/migrations/2017_04_26_211528_add_keywords_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddKeywordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keywords', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('keyword')->unique();
+            $table->integer('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('keywords');
+    }
+}
